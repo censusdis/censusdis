9489a3cb305c2608244c69a8382b5e33a037cc9e
vengroff <vengroff@gmail.com>
2022-11-07 13:32:16 +0000

    More test coverage.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import Any, Dict, Iterable
 
-from censusdis.data import VariableCache, VariableSource, _gf2s
+from censusdis.data import CensusApiVariableSource, VariableCache, VariableSource, _gf2s
 
 
 class TestFilters(unittest.TestCase):
@@ -331,6 +331,39 @@ class VariableCacheTestCase(unittest.TestCase):
 
                     for leaf_node in ethnicity_node.values():
                         self.assertTrue(leaf_node.is_leaf())
+
+
+class CensusApiVariableSourceTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self._variable_source = CensusApiVariableSource()
+        self._dataset = 'acs/acs5'
+        self._year = 2020
+        self._group_name = 'B01001'
+        self._name = f'{self._group_name}_001E'
+
+    def test_url(self):
+        url = self._variable_source.url(self._dataset, self._year, self._name)
+
+        self.assertEqual(
+            "https://api.census.gov/data/2020/acs/acs5/variables/B01001_001E.json",
+            url
+        )
+
+    def test_group_url(self):
+        url = self._variable_source.group_url(self._dataset, self._year)
+
+        self.assertEqual(
+            "https://api.census.gov/data/2020/acs/acs5/variables.json",
+            url
+        )
+
+        url = self._variable_source.group_url(self._dataset, self._year, self._group_name)
+
+        self.assertEqual(
+            "https://api.census.gov/data/2020/acs/acs5/groups/B01001.json",
+            url
+        )
 
 
 if __name__ == "__main__":
