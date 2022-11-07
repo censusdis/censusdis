38de5daa537ebf2f92e291f7eef053b5ea313fb3
vengroff <vengroff@gmail.com>
2022-11-07 13:47:28 +0000

    black

--- tests/test_data.py
+++ tests/test_data.py
@@ -334,35 +334,33 @@ class VariableCacheTestCase(unittest.TestCase):
 
 
 class CensusApiVariableSourceTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
         self._variable_source = CensusApiVariableSource()
-        self._dataset = 'acs/acs5'
+        self._dataset = "acs/acs5"
         self._year = 2020
-        self._group_name = 'B01001'
-        self._name = f'{self._group_name}_001E'
+        self._group_name = "B01001"
+        self._name = f"{self._group_name}_001E"
 
     def test_url(self):
         url = self._variable_source.url(self._dataset, self._year, self._name)
 
         self.assertEqual(
-            "https://api.census.gov/data/2020/acs/acs5/variables/B01001_001E.json",
-            url
+            "https://api.census.gov/data/2020/acs/acs5/variables/B01001_001E.json", url
         )
 
     def test_group_url(self):
         url = self._variable_source.group_url(self._dataset, self._year)
 
         self.assertEqual(
-            "https://api.census.gov/data/2020/acs/acs5/variables.json",
-            url
+            "https://api.census.gov/data/2020/acs/acs5/variables.json", url
         )
 
-        url = self._variable_source.group_url(self._dataset, self._year, self._group_name)
+        url = self._variable_source.group_url(
+            self._dataset, self._year, self._group_name
+        )
 
         self.assertEqual(
-            "https://api.census.gov/data/2020/acs/acs5/groups/B01001.json",
-            url
+            "https://api.census.gov/data/2020/acs/acs5/groups/B01001.json", url
         )
 
 
