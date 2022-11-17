d58599847160380fe065a02565c034d7f74a5585
vengroff <vengroff@gmail.com>
2022-11-17 16:55:57 -0600

    black

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -5,7 +5,6 @@ import censusdis.maps as cmap
 
 
 class ShapeReaderTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
         root = tempfile.TemporaryDirectory()
 
@@ -13,19 +12,29 @@ class ShapeReaderTestCase(unittest.TestCase):
         self.reader20 = cmap.ShapeReader(root, 2020)
 
     def test_url_for_file_09(self):
-        url = self.reader09._url_for_file('cb_something')
-        self.assertEqual('https://www2.census.gov/geo/tiger/GENZ2009/shp/cb_something.zip', url)
+        url = self.reader09._url_for_file("cb_something")
+        self.assertEqual(
+            "https://www2.census.gov/geo/tiger/GENZ2009/shp/cb_something.zip", url
+        )
 
-        url = self.reader09._url_for_file('tl_something')
-        self.assertEqual('https://www2.census.gov/geo/tiger/TIGER2009/SOMETHI/2009/tl_something.zip', url)
+        url = self.reader09._url_for_file("tl_something")
+        self.assertEqual(
+            "https://www2.census.gov/geo/tiger/TIGER2009/SOMETHI/2009/tl_something.zip",
+            url,
+        )
 
     def test_url_for_file_20(self):
-        url = self.reader20._url_for_file('cb_something')
-        self.assertEqual("https://www2.census.gov/geo/tiger/GENZ2020/shp/cb_something.zip", url)
+        url = self.reader20._url_for_file("cb_something")
+        self.assertEqual(
+            "https://www2.census.gov/geo/tiger/GENZ2020/shp/cb_something.zip", url
+        )
 
-        url = self.reader20._url_for_file('tl_something')
-        self.assertEqual("https://www2.census.gov/geo/tiger/TIGER2020/SOMETHING/tl_something.zip", url)
+        url = self.reader20._url_for_file("tl_something")
+        self.assertEqual(
+            "https://www2.census.gov/geo/tiger/TIGER2020/SOMETHING/tl_something.zip",
+            url,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
