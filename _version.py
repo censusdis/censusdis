de8f9daba08620cf7d412fbe4db3534eae6df2ea
Darren Erik Vengroff <vengroff@users.noreply.github.com>
2022-12-30 06:58:47 -0500

    Delete _version.py
    We don't actually use it and it is out of date.

--- _version.py
+++
@@ -1,7 +0,0 @@
-"""
-Global version number for any place in the project that needs it.
-"""
-
-# Full version including rc suffix if the is a release
-# candidate. E.g. 'X.Y.Z' or 'X.Y.Zrc'.
-release = "0.1.0rc"
