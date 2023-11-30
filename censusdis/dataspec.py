e67c76040415d6b2115fdc991c0369ebbb7d684c
vengroff <vengroff@gmail.com>
2023-11-30 10:29:37 -0500

    WIP: VariableSpec and subclasses.

---
+++ censusdis/dataspec.py
@@ -0,0 +1,167 @@
+from typing import List, Iterable, Optional, Tuple, Union
+import itertools
+import pandas as pd
+import geopandas as gpd
+
+import censusdis.data as ced
+from censusdis.geography import InSpecType
+from censusdis.impl.varsource.base import VintageType
+
+
+class VariableSpec:
+    def __init__(self, *, denominator: Union[str, bool] = False):
+        self._denominator = denominator
+
+    @property
+    def denominator(self) -> Union[str, bool]:
+        return self._denominator
+
+    def variables_to_download(self) -> List[str]:
+        if isinstance(self._denominator, str):
+            return [self._denominator]
+
+        return []
+
+    def groups_to_download(self) -> List[Tuple[str, bool]]:
+        return []
+
+    def synthesize(self, df_downloaded: Union[pd.DataFrame, gpd.GeoDataFrame]):
+        return df_downloaded
+
+    def download(
+        self,
+        dataset: str,
+        vintage: VintageType,
+        *,
+        set_to_nan: Union[bool, Iterable[int]] = True,
+        skip_annotations: bool = True,
+        with_geometry: bool = False,
+        remove_water: bool = False,
+        api_key: Optional[str] = None,
+        variable_cache: Optional["VariableCache"] = None,
+        row_keys: Optional[Union[str, Iterable[str]]] = None,
+        **kwargs: InSpecType,
+    ) -> Union[pd.DataFrame, gpd.GeoDataFrame]:
+        group_list = self.groups_to_download()
+
+        groups = [group for group, leaves_only in group_list if not leaves_only]
+        leaves_of_groups = [group for group, leaves_only in group_list if leaves_only]
+
+        if len(groups) == 0:
+            groups = None
+
+        if len(leaves_of_groups) == 0:
+            leaves_of_groups = None
+
+        df_or_gdf = ced.download(
+            dataset=dataset,
+            vintage=vintage,
+            download_variables=self.variables_to_download(),
+            group=groups,
+            leaves_of_group=leaves_of_groups,
+            set_to_nan=set_to_nan,
+            skip_annotations=skip_annotations,
+            with_geometry=with_geometry,
+            remove_water=remove_water,
+            api_key=api_key,
+            variable_cache=variable_cache,
+            row_keys=row_keys,
+            **kwargs,
+        )
+
+        return self.synthesize(df_or_gdf)
+
+
+class VariableList(VariableSpec):
+    def __init__(
+        self,
+        variables: Union[str, Iterable[str]],
+        *,
+        denominator: Union[str, bool] = False,
+    ):
+        super().__init__(denominator=denominator)
+        if isinstance(variables, str):
+            self._variables = [variables]
+        else:
+            self._variables = list(variables)
+
+    def variables_to_download(self) -> List[str]:
+        if (
+            isinstance(self.denominator, str)
+            and self.denominator not in self._variables
+        ):
+            # We specified a specific denominator that was not already
+            # one of the variables, so get it.
+            return self._variables + [self.denominator]
+        else:
+            # We don't need to fetch an extra variable for the denominator.
+            return self._variables
+
+    def synthesize(self, df_downloaded: Union[pd.DataFrame, gpd.GeoDataFrame]):
+        if not self.denominator:
+            return df_downloaded
+
+        if isinstance(self.denominator, str):
+            for variable in self._variables:
+                if variable != self.denominator:
+                    df_downloaded[f"frac_{variable}"] = (
+                        df_downloaded[variable] / df_downloaded[self.denominator]
+                    )
+        elif self.denominator:
+            denominator = df_downloaded[self._variables].sum(axis="columns")
+            for variable in self._variables:
+                df_downloaded[f"frac_{variable}"] = (
+                    df_downloaded[variable] / denominator
+                )
+
+
+class CensusGroup(VariableSpec):
+    def __init__(
+        self,
+        group: Union[str, Iterable[str]],
+        *,
+        leaves_only: bool = False,
+        denominator: Optional[str] = None,
+    ):
+        if denominator is None:
+            denominator = False
+
+        super().__init__(denominator=denominator)
+        self._group = [group] if isinstance(group, str) else list(group)
+        self._leaves_only = leaves_only
+
+    def groups_to_download(self) -> List[Tuple[str, bool]]:
+        return [(group, self._leaves_only) for group in self._group]
+
+
+class VariableSpecCollection(VariableSpec):
+    def __init__(self, variable_specs: Iterable[VariableSpec]):
+        super().__init__(denominator=None)
+        self._variable_specs = list(variable_specs)
+
+    def variables_to_download(self) -> List[str]:
+        return list(
+            set(
+                itertools.chain(
+                    *[spec.variables_to_download() for spec in self._variable_specs]
+                )
+            )
+        )
+
+    def groups_to_download(self) -> List[Tuple[str, bool]]:
+        return list(
+            set(
+                itertools.chain(
+                    *[spec.groups_to_download() for spec in self._variable_specs]
+                )
+            )
+        )
+
+    def synthesize(self, df_downloaded: Union[pd.DataFrame, gpd.GeoDataFrame]):
+        df = df_downloaded
+        for spec in self._variable_specs:
+            spec.synthesize(df)
+
+
+class DataSpec:
+    pass
