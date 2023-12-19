ff7fd5fe2dcff9204b7e6d2d68eaedf0a3909cb2
vengroff <vengroff@gmail.com>
2023-12-19 10:38:15 -0500

    Locate the version inside a function.

--- censusdis/__init__.py
+++ censusdis/__init__.py
@@ -10,26 +10,35 @@ from .impl.exceptions import CensusApiException
 import importlib.metadata
 from pathlib import Path
 
-version = "unknown"
 
-try:
-    # Try to get the version of the current package if
-    # it is running from a distribution.
-    package = Path(__file__).parent.name
-    _DISTRIBUTION_METADATA = importlib.metadata.metadata(package)
+def _package_version() -> str:
+    """Find the version of the package."""
+    package_version = "unknown"
 
-    version = _DISTRIBUTION_METADATA["Version"]
-except importlib.metadata.PackageNotFoundError:
-    # Fall back on getting it from a local pyproject.toml.
-    # This works in a development environment where the
-    # package has not been installed from a distribution.
-    import toml
+    try:
+        # Try to get the version of the current package if
+        # it is running from a distribution.
+        package = Path(__file__).parent.name
+        metadata = importlib.metadata.metadata(package)
 
-    pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
-    if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
-        version = toml.load(pyproject_toml_file)["tool"]["poetry"]["version"]
-        # Indicate it might be locally modified or unreleased.
-        version = version + "+"
+        package_version = metadata["Version"]
+    except importlib.metadata.PackageNotFoundError:
+        # Fall back on getting it from a local pyproject.toml.
+        # This works in a development environment where the
+        # package has not been installed from a distribution.
+        import toml
 
+        pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
+        if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
+            package_version = toml.load(pyproject_toml_file)["tool"]["poetry"][
+                "version"
+            ]
+            # Indicate it might be locally modified or unreleased.
+            package_version = package_version + "+"
+
+    return package_version
+
+
+version = _package_version()
 
 __all__ = ("CensusApiException", "version")
