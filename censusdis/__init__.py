a2c0ba87a4ca85efa703dd552cb0b8c3f8b5a1b9
Darren Erik Vengroff <vengroff@users.noreply.github.com>
2024-01-04 16:39:06 -0500

    One more optimization of censusdis.version. (#227)

--- censusdis/__init__.py
+++ censusdis/__init__.py
@@ -11,15 +11,23 @@ from .impl.exceptions import CensusApiException
 import importlib.metadata
 from pathlib import Path
 
+__package_version = "unknown"
 
-def _package_version() -> str:
+
+def __get_package_version() -> str:
     """Find the version of this package."""
-    package_version = "unknown"
+    global __package_version
+
+    if __package_version != "unknown":
+        # We already set it at some point in the past,
+        # so return that previous value without any
+        # extra work.
+        return __package_version
 
     try:
         # Try to get the version of the current package if
         # it is running from a distribution.
-        package_version = importlib.metadata.version("censusdis")
+        __package_version = importlib.metadata.version("censusdis")
     except importlib.metadata.PackageNotFoundError:
         # Fall back on getting it from a local pyproject.toml.
         # This works in a development environment where the
@@ -28,19 +36,19 @@ def _package_version() -> str:
 
         pyproject_toml_file = Path(__file__).parent.parent / "pyproject.toml"
         if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
-            package_version = toml.load(pyproject_toml_file)["tool"]["poetry"][
+            __package_version = toml.load(pyproject_toml_file)["tool"]["poetry"][
                 "version"
             ]
             # Indicate it might be locally modified or unreleased.
-            package_version = package_version + "+"
+            __package_version = __package_version + "+"
 
-    return package_version
+    return __package_version
 
 
 def __getattr__(name: str) -> Any:
     """Get package attributes."""
     if name in ("version", "__version__"):
-        return _package_version()
+        return __get_package_version()
     else:
         raise AttributeError(f"No attribute {name} in module {__name__}.")
 
