fe6d6181ddec91381f61aeaaeff91511f219389d
GitHub Action <action@github.com>
2024-06-14 05:28:01 +0000

    Update datasets [github action]

--- censusdis/datasets.py
+++ censusdis/datasets.py
@@ -673,7 +673,7 @@ DATASET_REFERENCE_URLS = {
     CPS_BASIC_JUL: "http://api.census.gov/data/2023/cps/basic/jul",
     CPS_BASIC_JUN: "http://api.census.gov/data/2023/cps/basic/jun",
     CPS_BASIC_MAR: "http://api.census.gov/data/2024/cps/basic/mar",
-    CPS_BASIC_MAY: "http://api.census.gov/data/2023/cps/basic/may",
+    CPS_BASIC_MAY: "http://api.census.gov/data/2024/cps/basic/may",
     CPS_BASIC_NOV: "http://api.census.gov/data/2023/cps/basic/nov",
     CPS_BASIC_OCT: "http://api.census.gov/data/2023/cps/basic/oct",
     CPS_BASIC_SEP: "http://api.census.gov/data/2023/cps/basic/sep",
